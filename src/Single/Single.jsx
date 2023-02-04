092752683771bbd59b925cd5f32ec4d811709886
Shehryar <shehryar7ahmed@gmail.com>
2023-02-04 08:06:41 +0500

    reservation-box-done

--- src/Single/Single.jsx
+++ src/Single/Single.jsx
@@ -13,6 +13,7 @@ import { Carousel } from "react-responsive-carousel";
 import MobileNav from "../Navbar/MobileNav";
 import "react-responsive-carousel/lib/styles/carousel.min.css"; // requires a loader
 import { useNavigate } from "react-router-dom";
+import "../Single/single.css";
 
 const Single = ({
   expand,
@@ -34,7 +35,7 @@ const Single = ({
   const [showCalendar, setShowcalendar] = useState(false);
   const [room, setRoom] = useState([]);
   const [show, setShow] = useState(false);
-  const [guestsInp, setGuestsInp] = useState(1);
+  const [guestsInp, setGuestsInp] = useState("1");
   const [reserve, setReserve] = useState(true);
   const [User, setUser] = useState([]);
   const [diffDays, setDiffDays] = useState(0);
@@ -48,8 +49,8 @@ const Single = ({
   const navigate = useNavigate();
 
   useEffect(() => {
-    console.log(reviews);
-  }, [reviews]);
+    console.log(range);
+  }, [range]);
   useEffect(() => {
     for (let index = 0; index < reviews.length; index++) {
       const element = reviews[index];
@@ -210,6 +211,9 @@ const Single = ({
     new Date(2022, 11, 23),
     new Date(2022, 11, 29),
   ];
+  useEffect(() => {
+    window.scrollTo(0, 0);
+  }, []);
   const wrapperRef = useRef(null);
   useOutsideAlerter(wrapperRef);
   return (
@@ -220,7 +224,7 @@ const Single = ({
           onClick={() => {
             navigate("/");
           }}
-          className="fa-solid fa-arrow-left mx-6   p-2 border border-gray-400 rounded-full bg-white  "
+          className="fa-solid fa-arrow-left mx-6  p-2 border border-gray-400 rounded-full bg-white  "
         ></i>
         <p className="text-lg font-semibold w-full     ">{room.category}</p>
         <div className="mx-10"></div>
@@ -284,16 +288,27 @@ const Single = ({
           ))}
         </Carousel>
       </div>
-      <div className="2md:flex      mt-6  mx-7">
-        <div className="  2md:px-24 outline">
-          <p className="text-2xl font-medium   ">
-            {room.houseType} hosted by {User.name}
-          </p>
-          <p className="">
-            {" "}
-            {room.guests} guests · {room.total_bedrooms} bedrooms ·{" "}
-            {room.total_bathrooms} baths · {room.total_occupancy} sq. ft
-          </p>
+      <div className="md:flex   justify-between   mt-6  mx-7">
+        <div className="  2md:px-24  ">
+          <div className="flex  items-center justify-between  w-[25rem] 2md:w-[30rem] md:w-[40rem]">
+            <div className="left__title">
+              <p className="text-2xl font-medium   ">
+                {room.houseType} hosted by {User.name}
+              </p>
+              <p className="">
+                {" "}
+                {room.guests} guests · {room.total_bedrooms} bedrooms ·{" "}
+                {room.total_bathrooms} baths · {room.total_occupancy} sq. ft
+              </p>
+            </div>
+            <div className="right__title__profile">
+              <img
+                src="https://t4.ftcdn.net/jpg/03/31/69/91/360_F_331699188_lRpvqxO5QRtwOM05gR50ImaaJgBx68vi.jpg"
+                className="h-14 w-14 rounded-full object-cover"
+                alt=""
+              />
+            </div>
+          </div>
           <br />
           <p className="font-medium text-2xl  w-96">What this place offers</p>
           <br />
@@ -385,8 +400,380 @@ const Single = ({
             )}
           </div>
         </div>
-        <div className="outline">
-          <h1>Hello</h1>
+        <div className="">
+          <div className="reserve__container h-[26rem] w-96  mx-auto p-5 mt-10 md:mt-0 shadow-xl border border-gray-300 rounded-xl">
+            <div className="top flex items-center justify-between ">
+              <div className="left text-xl flex items-center">
+                <p className="font-medium">${room.price}&nbsp;</p>
+                <p className="text-base mt-1">night</p>
+              </div>
+              <div className="right flex items-center">
+                <i class="fa-solid fa-star text-xs"></i>&nbsp;{" "}
+                <p className="flex items-center">
+                  <p className="text-sm font-medium">
+                    {reviewRatio > 0 ? (
+                      <>{Math.round(reviewRatio)} </>
+                    ) : (
+                      <>0&nbsp;</>
+                    )}
+                  </p>
+                  ·&nbsp;
+                  <span className="reviews span text-sm font-medium  text-[#828282] ">
+                    {reviews.length} reviews
+                  </span>
+                </p>
+              </div>
+            </div>
+            <div
+              onClick={() => {
+                setShowcalendar(true);
+              }}
+              className="box mt-8 flex  "
+            >
+              <input
+                type="text"
+                placeholder={`${range ? range.from : "Select"}`}
+                className={`border-t border-l   border-r outline-none   border-slate-600 h-10 p-2  rounded-tl-lg w-full ${
+                  showCalendar ? "border border-slate-900" : ""
+                } `}
+              />
+              <input
+                type="text"
+                placeholder={`${range ? range.to : "Select"}`}
+                className={`border-r border-t  outline-none   border-slate-600 h-10 p-2  rounded-tr-lg w-full ${
+                  showCalendar ? "border  border-slate-900" : ""
+                } `}
+              />
+            </div>
+            <div className="">
+              <input
+                type="text"
+                onChange={(e) => setGuestsInp(e.target.value)}
+                placeholder="1 Guest"
+                className="border border-slate-600 h-10 rounded-b-lg p-2  w-full"
+              />
+            </div>
+            {showCalendar ? (
+              <div
+                ref={wrapperRef}
+                className="day-picker bg-slate-400 absolute top-[80rem]"
+                style={{
+                  width: "279px",
+                  paddingLeft: "-20",
+                  margin: "0",
+                }}
+              >
+                <p
+                  style={{
+                    margin: "-0px",
+                    paddingLeft: "10px",
+                    paddingTop: "0px",
+                  }}
+                ></p>
+                <DayPicker
+                  style={{ marginLeft: "-1px", marginTop: "-3px" }}
+                  mode="range"
+                  selected={range}
+                  onSelect={setRange}
+                  disabled={disabled}
+                />
+              </div>
+            ) : null}
+            <br />
+            {showCalendar ? null : reserve ? (
+              <div
+                className="btn-container"
+                onClick={() => {
+                  if (!range) {
+                    setReserve(true);
+                    return toast.error("Please select a date");
+                  }
+                  setReserve(false);
+                  const requestOptions = {
+                    method: "POST",
+                    headers: {
+                      "Content-Type": "application/json",
+                      token: `${localStorage.getItem("token")}`,
+                    },
+                    body: JSON.stringify({
+                      dateStart: range.from,
+                      dateEnd: range.to,
+                      room: room._id,
+                      guests: guestsInp,
+                    }),
+                  };
+                  fetch("http://localhost:7000/reserve", requestOptions)
+                    .then(async (response) => {
+                      const isJson = response.headers
+                        .get("content-type")
+                        ?.includes("application/json");
+                      const data = isJson && (await response.json());
+                      setReserve(true);
+                      // check for error response
+                      if (!response.ok) {
+                        // get error message from body or default to response status
+
+                        setReserve(true);
+
+                        return toast.error("Please add all the fields");
+                      }
+                      toast.success("Your reservation has been made!");
+                    })
+                    .catch((error) => {
+                      console.error("There was an error!", error);
+                    });
+                }}
+              >
+                <div className="cell"> </div>
+                <div className="cell"> </div>
+                <div className="cell"> </div>
+                <div className="cell"> </div>
+                <div className="cell"> </div>
+                <div className="cell"> </div>
+                <div className="cell"> </div>
+                <div className="cell"> </div>
+                <div className="cell"> </div>
+                <div className="cell"> </div>
+                <div className="cell"> </div>
+                <div className="cell"> </div>
+                <div className="cell"> </div>
+                <div className="cell"> </div>
+                <div className="cell"> </div>
+                <div className="cell"> </div>
+                <div className="cell"> </div>
+                <div className="cell"> </div>
+                <div className="cell"> </div>
+                <div className="cell"> </div>
+                <div className="cell"> </div>
+                <div className="cell"> </div>
+                <div className="cell"> </div>
+                <div className="cell"> </div>
+                <div className="cell"> </div>
+                <div className="cell"> </div>
+                <div className="cell"> </div>
+                <div className="cell"> </div>
+                <div className="cell"> </div>
+                <div className="cell"> </div>
+                <div className="cell"> </div>
+                <div className="cell"> </div>
+                <div className="cell"> </div>
+                <div className="cell"> </div>
+                <div className="cell"> </div>
+                <div className="cell"> </div>
+                <div className="cell"> </div>
+                <div className="cell"> </div>
+                <div className="cell"> </div>
+                <div className="cell"> </div>
+                <div className="cell"> </div>
+                <div className="cell"> </div>
+                <div className="cell"> </div>
+                <div className="cell"> </div>
+                <div className="cell"> </div>
+                <div className="cell"> </div>
+                <div className="cell"> </div>
+                <div className="cell"> </div>
+                <div className="cell"> </div>
+                <div className="cell"> </div>
+                <div className="cell"> </div>
+                <div className="cell"> </div>
+                <div className="cell"> </div>
+                <div className="cell"> </div>
+                <div className="cell"> </div>
+                <div className="cell"> </div>
+                <div className="cell"> </div>
+                <div className="cell"> </div>
+                <div className="cell"> </div>
+                <div className="cell"> </div>
+                <div className="cell"> </div>
+                <div className="cell"> </div>
+                <div className="cell"> </div>
+                <div className="cell"> </div>
+                <div className="cell"> </div>
+                <div className="cell"> </div>
+                <div className="cell"> </div>
+                <div className="cell"> </div>
+                <div className="cell"> </div>
+                <div className="cell"> </div>
+                <div className="cell"> </div>
+                <div className="cell"> </div>
+                <div className="cell"> </div>
+                <div className="cell"> </div>
+                <div className="cell"> </div>
+                <div className="cell"> </div>
+                <div className="cell"> </div>
+                <div className="cell"> </div>
+                <div className="cell"> </div>
+                <div className="cell"> </div>
+                <div className="cell"> </div>
+                <div className="cell"> </div>
+                <div className="cell"> </div>
+                <div className="cell"> </div>
+                <div className="cell"> </div>
+                <div className="cell"> </div>
+                <div className="cell"> </div>
+                <div className="cell"> </div>
+                <div className="cell"> </div>
+                <div className="cell"> </div>
+                <div className="cell"> </div>
+                <div className="cell"> </div>
+                <div className="cell"> </div>
+                <div className="cell"> </div>
+                <div className="cell"> </div>
+                <div className="cell"> </div>
+                <div className="cell"> </div>
+                <div className="cell"> </div>
+                <div className="cell"> </div>
+                <div className="cell"> </div>
+                <div className="content">
+                  <button className="action-btn">
+                    <span className="span">Reserve</span>
+                  </button>
+                </div>
+                {/* <br />
+<br /> */}
+              </div>
+            ) : (
+              <div className="btn-container">
+                <div className="cell"> </div>
+                <div className="cell"> </div>
+                <div className="cell"> </div>
+                <div className="cell"> </div>
+                <div className="cell"> </div>
+                <div className="cell"> </div>
+                <div className="cell"> </div>
+                <div className="cell"> </div>
+                <div className="cell"> </div>
+                <div className="cell"> </div>
+                <div className="cell"> </div>
+                <div className="cell"> </div>
+                <div className="cell"> </div>
+                <div className="cell"> </div>
+                <div className="cell"> </div>
+                <div className="cell"> </div>
+                <div className="cell"> </div>
+                <div className="cell"> </div>
+                <div className="cell"> </div>
+                <div className="cell"> </div>
+                <div className="cell"> </div>
+                <div className="cell"> </div>
+                <div className="cell"> </div>
+                <div className="cell"> </div>
+                <div className="cell"> </div>
+                <div className="cell"> </div>
+                <div className="cell"> </div>
+                <div className="cell"> </div>
+                <div className="cell"> </div>
+                <div className="cell"> </div>
+                <div className="cell"> </div>
+                <div className="cell"> </div>
+                <div className="cell"> </div>
+                <div className="cell"> </div>
+                <div className="cell"> </div>
+                <div className="cell"> </div>
+                <div className="cell"> </div>
+                <div className="cell"> </div>
+                <div className="cell"> </div>
+                <div className="cell"> </div>
+                <div className="cell"> </div>
+                <div className="cell"> </div>
+                <div className="cell"> </div>
+                <div className="cell"> </div>
+                <div className="cell"> </div>
+                <div className="cell"> </div>
+                <div className="cell"> </div>
+                <div className="cell"> </div>
+                <div className="cell"> </div>
+                <div className="cell"> </div>
+                <div className="cell"> </div>
+                <div className="cell"> </div>
+                <div className="cell"> </div>
+                <div className="cell"> </div>
+                <div className="cell"> </div>
+                <div className="cell"> </div>
+                <div className="cell"> </div>
+                <div className="cell"> </div>
+                <div className="cell"> </div>
+                <div className="cell"> </div>
+                <div className="cell"> </div>
+                <div className="cell"> </div>
+                <div className="cell"> </div>
+                <div className="cell"> </div>
+                <div className="cell"> </div>
+                <div className="cell"> </div>
+                <div className="cell"> </div>
+                <div className="cell"> </div>
+                <div className="cell"> </div>
+                <div className="cell"> </div>
+                <div className="cell"> </div>
+                <div className="cell"> </div>
+                <div className="cell"> </div>
+                <div className="cell"> </div>
+                <div className="cell"> </div>
+                <div className="cell"> </div>
+                <div className="cell"> </div>
+                <div className="cell"> </div>
+                <div className="cell"> </div>
+                <div className="cell"> </div>
+                <div className="cell"> </div>
+                <div className="cell"> </div>
+                <div className="cell"> </div>
+                <div className="cell"> </div>
+                <div className="cell"> </div>
+                <div className="cell"> </div>
+                <div className="cell"> </div>
+                <div className="cell"> </div>
+                <div className="cell"> </div>
+                <div className="cell"> </div>
+                <div className="cell"> </div>
+                <div className="cell"> </div>
+                <div className="cell"> </div>
+                <div className="cell"> </div>
+                <div className="cell"> </div>
+                <div className="cell"> </div>
+                <div className="cell"> </div>
+                <div className="cell"> </div>
+                <div className="cell"> </div>
+                <div className="cell"> </div>
+                <div className="content">
+                  <button className="action-btn">
+                    <span className="span">
+                      <div class="lds-ellipsis">
+                        <div></div>
+                        <div></div>
+                        <div></div>
+                        <div></div>
+                      </div>
+                    </span>
+                  </button>
+                </div>
+                {/* <br />
+    <br /> */}
+              </div>
+            )}
+            <br />
+            <div className="price__bottom__reservation__box">
+              <div className="top flex  text-lg  justify-between">
+                <p className="underline">
+                  ${room.price} x {diffDays} nights
+                </p>
+                <p>${room.price * diffDays} </p>
+              </div>
+              <div className="service my-4">
+                <div className="flex justify-between text-lg">
+                  <p className="underline">Service fee</p>
+                  <p>12%</p>
+                </div>
+              </div>
+              <hr />
+              <div className="total_price flex justify-between text-lg my-5 font-semibold">
+                <p>Total after taxes</p>
+                <p>
+                  $
+                  {room.price * diffDays + ((room.price * diffDays) / 100) * 12}
+                </p>
+              </div>
+            </div>
+          </div>
         </div>
       </div>
     </div>
